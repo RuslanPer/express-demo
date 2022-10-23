455d174f431341b3f1994d1f20a4828393f14bf5
Ruslan Pershin <ruslan.pershin98@gmail.com>
2022-10-23 21:18:49 +0300

    feat: Express middleware, chain of responsibility

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,8 @@ import express, { Request, Response } from 'express'
 import bodyParser from 'body-parser'
 import { productsRouter } from './routes/products-router'
 import { addressesRouter } from './routes/addresses-router'
+import { NextFunction } from 'express-serve-static-core'
+import { read } from 'fs'
 
 // create express app
 const app = express()
@@ -9,6 +11,32 @@ const port = process.env.PORT || 5000
 
 const parserMiddleware = bodyParser({})
 app.use(parserMiddleware)
+
+const authGuardMiddleware = (
+	req: Request,
+	res: Response,
+	next: NextFunction
+) => {
+	if (req.query.token === '123') {
+		next()
+	} else {
+		res.send(401)
+	}
+}
+
+let requestCounter = 0
+
+const requestCounterMiddleware = (
+	req: Request,
+	res: Response,
+	next: NextFunction
+) => {
+	requestCounter++
+	next()
+}
+
+app.use(authGuardMiddleware)
+app.use(requestCounterMiddleware)
 
 app.use('/products', productsRouter)
 app.use('/addresses', addressesRouter)
